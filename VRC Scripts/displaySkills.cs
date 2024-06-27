2fe63f4ea5ad842fcc27e7f2e7116abcd850eecc
Dawgy <160785597+DawgyMew@users.noreply.github.com>
2024-06-27 00:57:58 -0500

    show skills

---
+++ VRC Scripts/displaySkills.cs
@@ -0,0 +1,39 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
+public class displaySkills : UdonSharpBehaviour
+{
+    public Dictionaries dictionary;
+    public TextMeshProUGUI txtBox;
+
+
+    void Awake(){
+        dictionary = GameObject.Find("Dictionary").GetComponent<Dictionaries>();
+    }
+    public override void OnPickup(){
+        if (GetComponent<VRC.SDKBase.VRC_Pickup>().currentPlayer.isLocal){
+            string playerName = GetComponent<VRC.SDKBase.VRC_Pickup>().currentPlayer.displayName;
+            Debug.Log(playerName);
+            var playerSkills = Dictionaries.getArray(dictionary.self, playerName, "Skills", "Name"); // get the array of skills the player has
+            int i = 1;
+            string leftText = "";
+            foreach (var skill in playerSkills){
+                // check if the skill is passive or not //
+                var skillCost = Dictionaries.getSkillInfo(dictionary, skill)["Cost"].Int;
+                if (skillCost != 0){
+                    if (skillCost != -1){
+                        leftText += skill + " \t" + skillCost + "\n";
+                    }
+                    else{
+                        leftText += "null\n";
+                    }
+                    i++;
+                }
+            }
+            txtBox.text = leftText;
+        }
+    }
+}
--- VRC Scripts/showMenu.cs
+++ VRC Scripts/showMenu.cs
@@ -10,17 +10,19 @@ public class showMenu : UdonSharpBehaviour
     public GameObject menu;
     
     public override void OnPickup(){
-        VRC_Pickup vrc_pickup = GetComponent<VRC.SDKBase.VRC_Pickup>(); // get the vrc pickup script to use
-        var hand = vrc_pickup.currentHand; // print the hand that is holding the evoker
-        
-        menu.SetActive(true); // enable the menu
+        if (GetComponent<VRC.SDKBase.VRC_Pickup>().currentPlayer.isLocal){// only displays the menu for the local user
+            VRC_Pickup vrc_pickup = GetComponent<VRC.SDKBase.VRC_Pickup>(); // get the vrc pickup script to use
+            var hand = vrc_pickup.currentHand; // print the hand that is holding the evoker
+            
+            menu.SetActive(true); // enable the menu
 
-        int dirMult = 1;
-        // move the menu based on which hand is holding it //
-        if (hand == VRC_Pickup.PickupHand.Left){ // makes it match what current hand returns
-            dirMult = -1;
+            int dirMult = 1;
+            // move the menu based on which hand is holding it //
+            if (hand == VRC_Pickup.PickupHand.Left){ // makes it match what current hand returns
+                dirMult = -1;
+            }
+            menu.transform.localPosition = new Vector3(.29f * dirMult, .12f, 0);
         }
-        menu.transform.localPosition = new Vector3(.29f * dirMult, .12f, 0);
     }
     public override void OnDrop(){
         menu.SetActive(false);
