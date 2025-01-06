e56924c8dacba028befa35a24c5c7cd3404eeed8
Dawgy <160785597+DawgyMew@users.noreply.github.com>
2025-01-06 16:51:51 -0600

    plink

---
+++ enemyAI.cs
@@ -0,0 +1,11 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class enemyAI : UdonSharpBehaviour
+{
+    /// Class that determines the moves of the enemies ///
+    
+}
