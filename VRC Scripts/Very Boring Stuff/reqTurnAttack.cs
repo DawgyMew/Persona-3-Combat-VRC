8aa258ac62dde4cdf76b70498bef5de818f0192f
Dawgy <160785597+DawgyMew@users.noreply.github.com>
2025-01-16 10:49:02 -0600

    require turn to attack

---
+++ VRC Scripts/Very Boring Stuff/reqTurnAttack.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class reqTurnAttack : UdonSharpBehaviour
+{   
+    public turnLogic tl;
+    public Material on;
+    public Material off;
+    
+    public override void OnPlayerJoined(VRCPlayerApi player){
+        if (tl.requireTurnToAttack){
+            this.GetComponent<MeshRenderer> ().material = on;
+
+        }
+        else{
+            this.GetComponent<MeshRenderer> ().material = off;
+        }
+    }
+
+}
